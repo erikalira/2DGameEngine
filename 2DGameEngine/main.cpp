3c72c461b7f04683e129d1c4461af9d59fe7f239
Erika <liraerika00@gmail.com>
2019-12-27 15:52:07 -0300

    loop em tela cheia

---
+++ 2DGameEngine/main.cpp
@@ -0,0 +1,20 @@
+#include "Game.h"
+
+Game* game = nullptr;
+
+int main(int argc, char* argv[]) {
+
+	game = new Game();
+
+	game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, true);
+
+	while (game->running()) {
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+
+	game->clean();
+
+	return 0;
+}
