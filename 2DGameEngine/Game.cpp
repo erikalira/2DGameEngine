6087f2cb3346b0bd175cc656302d29743e7dc6c6
Erika <liraerika00@gmail.com>
2020-01-01 05:01:36 -0300

    assets, sdl image e movimentos "ao infinito e alem"

--- 2DGameEngine/Game.cpp
+++ 2DGameEngine/Game.cpp
@@ -1,5 +1,8 @@
 #include "Game.h"
 
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
+
 Game::Game() {
 
 }
@@ -14,25 +17,18 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		flags = SDL_WINDOW_FULLSCREEN;
 	}
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-		std::cout << "Subsystem initilised!..." << std::endl;
-
 		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-
-		if (window) {
-			std::cout << "window created!..." << std::endl;
-		}
-
 		renderer = SDL_CreateRenderer(window, -1, 0);
 		if (renderer) {
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			std::cout << "renderer created!..." << std::endl;
 		}
 
 		isRunning = true;
 	}
-	else {
-		isRunning = false;
-	}
+	
+	SDL_Surface* tmpSurface = IMG_Load("assets/detective/Idle/Idle_02.png");
+	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
 }
 
 void Game::handleEvents() {
@@ -51,12 +47,16 @@ void Game::handleEvents() {
 
 void Game::update() {
 	count++;
+	destR.h = 64;
+	destR.w = 64;
+	destR.x = count;
+	destR.y = 300;
 	std::cout << count << std::endl;
 }
 
 void Game::render() {
 	SDL_RenderClear(renderer);
-	//add stuff to render
+	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
 	SDL_RenderPresent(renderer);
 }
 
--- 2DGameEngine/main.cpp
+++ 2DGameEngine/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
 
 	game = new Game();
 
-	game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, true);
+	game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, false);
 
 	while (game->running()) {
 		game->handleEvents();
